6531944babc28ed7370b54be86e04b234d3e09aa
Turan Naimey <turan1naimey@gmail.com>
2018-10-17 21:45:03 -0700

    DEV: ElectronJS setup

---
+++ main.js
@@ -0,0 +1,26 @@
+const {app, BrowserWindow} = require('electron')
+
+function createWindow(){
+  wind = new BrowserWindow({width: 800, heigth: 600})
+
+  wind.loadFile('index.html')
+
+  wind.on('closed', () => {wind=null})
+
+}
+
+
+app.on('ready', createWindow)
+
+app.on('windows-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+
+app.on('activate', () => {
+  if (wind === null) {
+    createWindow()
+  }
+})
