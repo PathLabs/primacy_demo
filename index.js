598f0444d8dd76271c9b7fd2349eef7734caee72
skate54345 <ak678@iris.nau.edu>
2018-10-25 15:21:31 -0700

    fix minor dependency issues

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const electron = require('electron')
+// const tabGroup = require("electron-tabs");
 const app = electron.app
 
 const BrowserWindow = electron.BrowserWindow
 var mainWindow
+
 app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 1024, height:768, backgroundColor: '#000'})
   mainWindow.loadURL('file:///'+ __dirname + '/index.html')
   console.log('file:///'+ __dirname + '/index.html')
 });
 
-// const TabGroup = require("electron-tabs");
-//
 // // Initializes tab group //
 // let tabGroup = new TabGroup();
 // let tab = tabGroup.addTab({
--- main.js
+++ main.js
@@ -1,26 +1,29 @@
-const {app, BrowserWindow} = require('electron')
 
-function createWindow(){
-  wind = new BrowserWindow({width: 800, heigth: 600})
+//Not sure where this came from, currently not needed
 
-  wind.loadFile('index.html')
-
-  wind.on('closed', () => {wind=null})
-
-}
-
-
-app.on('ready', createWindow)
-
-app.on('windows-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-
-app.on('activate', () => {
-  if (wind === null) {
-    createWindow()
-  }
-})
+// const {app, BrowserWindow} = require('electron')
+//
+// function createWindow(){
+//   wind = new BrowserWindow({width: 800, heigth: 600})
+//
+//   wind.loadFile('index.html')
+//
+//   wind.on('closed', () => {wind=null})
+//
+// }
+//
+//
+// app.on('ready', createWindow)
+//
+// app.on('windows-all-closed', () => {
+//   if (process.platform !== 'darwin') {
+//     app.quit()
+//   }
+// })
+//
+//
+// app.on('activate', () => {
+//   if (wind === null) {
+//     createWindow()
+//   }
+// })
