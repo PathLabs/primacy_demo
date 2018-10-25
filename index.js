d635855f608c5118d25256da513ac5997398ada4
skate54345 <ak678@iris.nau.edu>
2018-10-25 14:56:35 -0700

    index js setup

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const TabGroup = require("electron-tabs");
+const electron = require('electron')
+const app = electron.app
 
-
-// Initializes tab group //
-let tabGroup = new TabGroup();
-let tab = tabGroup.addTab({
-    title: "Electron",
-    src: "http://electron.atom.io",
-    visible: true
+const BrowserWindow = electron.BrowserWindow
+var mainWindow
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({width: 1024, height:768, backgroundColor: '#000'})
+  mainWindow.loadURL('file:///'+ __dirname + '/index.html')
+  console.log('file:///'+ __dirname + '/index.html')
 });
+
+// const TabGroup = require("electron-tabs");
+//
+// // Initializes tab group //
+// let tabGroup = new TabGroup();
+// let tab = tabGroup.addTab({
+//     title: "Electron",
+//     src: "http://electron.atom.io",
+//     visible: true
+// });
