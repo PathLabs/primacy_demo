c690e0f2f68159088a614eb93a057086a8342a04
austin <skate54345@gmail.com>
2018-12-07 00:29:31 -0700

    created variables for tab1 GUI elements

--- tab1.js
+++ tab1.js
@@ -1,5 +1,7 @@
 const submitButton = document.getElementById("submitButton")
+var startRange = document.getElementById("startRange")
+var endRange = document.getElementById("endRange")
 
 submitButton.addEventListener('click', function () {
-
+  //if (startRange
 }
