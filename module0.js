5b1178aff9874fb91676baf31847cdb92e643dc7
Chance Nelson <chance-nelson@nau.edu>
2019-01-31 16:35:54 -0700

    Add nucleotide sequence box and file input

--- module0.js
+++ module0.js
@@ -7,16 +7,18 @@
  *      - Chance Nelson <chance-nelson@nau.edu>
  */
 
-const module0      = document.getElementById("module0");
-const module1      = document.getElementById("module1");
-const submitButton = document.getElementById("submitButton");
-const fileSelect   = document.getElementById("file_select");
-var lowerRange     = document.getElementById("startRange");
-var endRange       = document.getElementById("endRange");
+const module0             = document.getElementById("module0");
+const module1             = document.getElementById("module1");
+const submit_button       = document.getElementById("submitButton");
+const fasta_file_select   = document.getElementById("fastaFileSelect");
+const fasta_file_textarea = document.getElementById("fastaTextInput");
+var lowerRange            = document.getElementById("startRange");
+var endRange              = document.getElementById("endRange");
 
 
 //gets users home directory
 const os = require('os');
+const fs = require('fs');
 
 var validate = require('./lib/input_validation.js');
 
@@ -49,7 +51,7 @@ ipcRenderer.on('EXECUTE', (event, arg) =>{
 
 ipcRenderer.on('NEW', (event, arg) =>{
     console.log("NEW received");
-    console.log(fileSelect.value);
+    console.log(fasta_file_select.value);
     init(arg[0]);
 });
 
@@ -80,3 +82,37 @@ submitButton.addEventListener('click', function () {
         console.log(e);
     }
 });
+
+fasta_file_select.addEventListener('change', function() {
+    /**
+     * Desc: Reads in a FASTA file and displays it in the
+     *       textarea.
+     */
+    console.log("FASTA file change");
+
+    // Read in the data from the file
+    fs.readFile(fasta_file_select.files[0].path, function(err, data) {
+        if(err) {
+            console.log("FASTA file read error");
+        }
+
+        
+        fasta_file_textarea.value = data.toString();
+
+        // Force the text area's change event to fire
+        let change_event = new Event("change");
+        fasta_file_textarea.dispatchEvent(change_event);
+    });
+
+});
+
+fasta_file_textarea.addEventListener('change', function() {
+    /**
+     * Desc: Handles fasta sequence changes. Validates the input
+     *       and then sets the fasta_sequence variable
+     */
+    console.log("FASTA sequence textbox change");
+
+    // TODO: Validate current fasta sequence
+});
+
