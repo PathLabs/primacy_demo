56e6d6250cef1f32c8faaf5f098800e6e7d81adc
skate54345 <ak678@iris.nau.edu>
2019-03-12 13:38:35 -0700

    added dialog box on window close

--- main.js
+++ main.js
@@ -33,20 +33,29 @@ function initial() {
     win = new BrowserWindow({width: 1024, height:768, backgroundColor: '#000'});
 
     // and load the index.html of the app.
-    win.loadURL('file:///'+ __dirname + '/src/html/module2.html');
+    win.loadURL('file:///'+ __dirname + '/src/html/module1.html');
 
     // Open the DevTools.
     // win.webContents.openDevTools()
 
     // Emitted when the window is closed.
-    win.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
+    win.on('close', function(e){
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+        var choice = require('electron').dialog.showMessageBox(this,
+        {
+            type: 'question',
+            buttons: ['Yes', 'No'],
+            title: 'Confirm',
+            message: 'Are you sure you want to quit?'
+        });
+        if(choice == 1){
+            e.preventDefault();
+        }
         win = null;
     });
 }
-
 
 function goToModule(module_number) {
     /**
