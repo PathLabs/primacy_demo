e26b271ad806ebab844c0dab6025c0e6e8839a6e
Turan Naimey <turan1naimey@gmail.com>
2019-03-14 18:09:54 -0700

    Parse JSON and add test data

--- src/js/viz.js
+++ src/js/viz.js
@@ -1,11 +1,28 @@
+const fs = require('fs');
+
+
 var data =
     JSON.parse(
-        require('fs').readFileSync(
+        fs.readFileSync(
             require('path').resolve(
                 __dirname,
                 'test_input.json'),
             'utf8'));
 
 
-console.log(typeof(data))
+
 var sequence_id = Object.keys(data)[0]
+
+
+var primer_ids = Object.keys(data[sequence_id].forward)
+// console.log(data[sequence_id]['forward']['Sa_692846_245_18_F']['gc'])
+
+var i;
+
+var result_gc = []
+
+for (i = 0; i< primer_ids.length; i++ ){
+  primer_id = primer_ids[i]
+
+  result_gc.push(data[sequence_id].forward[primer_id].gc)
+}
