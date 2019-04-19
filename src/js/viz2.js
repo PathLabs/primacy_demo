381e65ce7195645e089c2bc1d0ce569a4d61d687
Turan Naimey <turan1naimey@gmail.com>
2019-04-19 05:32:59 -0700

    Working Bulk Count

--- src/js/viz2.js
+++ src/js/viz2.js
@@ -3,24 +3,23 @@ const path = require("path");
 const os = require("os");
 const { ipcRenderer } = require("electron");
 // store score values for updating primer counts in the viz
-var scores = {'forward': {},
-              'reverse': {}
+var scores = {
+  forward: { values: [] },
+  reverse: { values: [] }
 };
 
 // pipeline output after running module 2
 var pipeline_mod_2_output = "Sa_50_collection_out.json";
 
-
 /**
-* Desc: This function extracts paths for every single sequence JSON file created after module 2
-*
-* Args:
-*     Path to the module 2 output JSON
-*
-* Returns:
-*      Array containing paths for every single sequence JSON created by module 1
-*/
-
+ * Desc: This function extracts paths for every single sequence JSON file created after module 2
+ *
+ * Args:
+ *     Path to the module 2 output JSON
+ *
+ * Returns:
+ *      Array containing paths for every single sequence JSON created by module 1
+ */
 
 function get_paths(json_file_path) {
   var collection_out = JSON.parse(
@@ -40,42 +39,41 @@ function get_paths(json_file_path) {
 }
 
 /**
-* Desc: This function parses information for the viz from every single sequence JSON
-*       created by module 2.
-*
-* Args:
-*     direction (string): this is either 'reverse' or 'forward' and is used to find
-*                         respective directional information from the JSON
-*
-* Returns:
-*      Array containing two arrays with x and y values where x is the sequence names and y is the values for the
-*      score values (x and y must be the same length and every x value must match the y value)
-*/
+ * Desc: This function parses information for the viz from every single sequence JSON
+ *       created by module 2.
+ *
+ * Args:
+ *     direction (string): this is either 'reverse' or 'forward' and is used to find
+ *                         respective directional information from the JSON
+ *
+ * Returns:
+ *      Array containing two arrays with x and y values where x is the sequence names and y is the values for the
+ *      score values (x and y must be the same length and every x value must match the y value)
+ */
 
 function parse_data(direction) {
   paths_array = get_paths(pipeline_mod_2_output);
   var xData = [];
   var yData = [];
 
-
   if (direction === "forward") {
     for (let i = 0; i < paths_array.length; i++) {
       let data = JSON.parse(
         fs.readFileSync(path.resolve(paths_array[i]), "UTF-8")
       );
-        let sequence_id = Object.keys(data)[0];
-        let values = [];
-        scores.forward[sequence_id] = {};
-        xData.unshift(sequence_id);
-        forward_primers = Object.keys(data[sequence_id].forward);
-        for (let k = 0; k < forward_primers.length; k++) {
-          score = data[sequence_id].forward[forward_primers[k]]['score'];
-          primer_id = forward_primers[k];
-          values.push(score);
-          scores.forward[sequence_id][primer_id] = score;
-        }
-        yData.unshift(values);
-
+      let sequence_id = Object.keys(data)[0];
+      let values = [];
+      scores.forward[sequence_id] = {};
+      xData.unshift(sequence_id);
+      forward_primers = Object.keys(data[sequence_id].forward);
+      for (let k = 0; k < forward_primers.length; k++) {
+        score = data[sequence_id].forward[forward_primers[k]]["score"];
+        primer_id = forward_primers[k];
+        scores.forward.values.push(score);
+        values.push(score);
+        scores.forward[sequence_id][primer_id] = score;
+      }
+      yData.unshift(values);
     }
   }
 
@@ -85,24 +83,23 @@ function parse_data(direction) {
         fs.readFileSync(path.resolve(paths_array[i]), "UTF-8")
       );
 
-
-        var sequence_id = Object.keys(data)[0];
-        if (data[sequence_id].hasOwnProperty("reverse")) {
-          let values = [];
-          scores.reverse[sequence_id] = {};
-          xData.unshift(sequence_id);
-          reverse_primers = Object.keys(data[sequence_id].reverse);
-          for (let k = 0; k < reverse_primers.length; k++) {
-            score = data[sequence_id].reverse[reverse_primers[k]]['score'];
-            primer_id = reverse_primers[k];
-            values.push(score);
-            scores.reverse[sequence_id][primer_id] = score;
-          }
-          yData.unshift(values);
-        } else {
-          continue;
+      var sequence_id = Object.keys(data)[0];
+      if (data[sequence_id].hasOwnProperty("reverse")) {
+        let values = [];
+        scores.reverse[sequence_id] = {};
+        xData.unshift(sequence_id);
+        reverse_primers = Object.keys(data[sequence_id].reverse);
+        for (let k = 0; k < reverse_primers.length; k++) {
+          score = data[sequence_id].reverse[reverse_primers[k]]["score"];
+          primer_id = reverse_primers[k];
+          scores.reverse.values.push(score);
+          values.push(score);
+          scores.reverse[sequence_id][primer_id] = score;
         }
-
+        yData.unshift(values);
+      } else {
+        continue;
+      }
     }
   }
 
@@ -110,16 +107,15 @@ function parse_data(direction) {
 }
 
 /**
-* Desc: This function creates the d3 based plotly spec which will render the viz
-*
-* Args:
-*     Since this function will be called from within the HTML, the arguments
-*     are direction and div (direction to be passed to parse_data())
-*
-* Returns:
-*      None
-*/
-
+ * Desc: This function creates the d3 based plotly spec which will render the viz
+ *
+ * Args:
+ *     Since this function will be called from within the HTML, the arguments
+ *     are direction and div (direction to be passed to parse_data())
+ *
+ * Returns:
+ *      None
+ */
 
 function create_viz_spec(direction, div) {
   data = parse_data(direction);
@@ -159,12 +155,12 @@ function create_viz_spec(direction, div) {
   }
 
   layout = {
-    title: 'Score' + " Box Plot Chart for " + direction + " primers",
+    title: "Score" + " Box Plot Chart for " + direction + " primers",
     autosize: false,
     width: 800,
     height: 450,
     yaxis: {
-      title: 'Score',
+      title: "Score",
       autorange: true,
       showgrid: true,
       zeroline: true,
@@ -174,10 +170,10 @@ function create_viz_spec(direction, div) {
       zerolinecolor: "rgb(255, 255, 255)",
       zerolinewidth: 2
     },
-    xaxis:{
-      title: 'Sequence ID',
+    xaxis: {
+      title: "Sequence ID",
       autorange: true,
-      tickmode: 'linear'
+      tickmode: "linear"
     },
     margin: {
       l: 50,
@@ -195,28 +191,31 @@ function create_viz_spec(direction, div) {
 
   Plotly.newPlot(div, data, layout);
 }
-function update_count_all(direction, percent_val){
-    // store all score values in an array
-    scores_arr = []
 
-    for (let i = 0; i < Object.keys(scores[direction]).length; i++) {
-      sequence_id = Object.keys(scores[direction])[i]
-      for (let j = 0; j < Object.keys(scores[direction][sequence_id]).length; j++){
 
-        // push every single score value in this array
-        primer_ids = Object.keys(scores[direction][sequence_id])
-        scores_arr.unshift(scores[direction][sequence_id][primer_id[j]]);
-      }
-    }
+function update_count_all(direction, percent_val) {
+  score_values = scores[direction].values;
+  // sort the array of values
+  score_values.sort(function(a, b) {
+  return a - b;
+  });
 
-    console.log(scores_arr.length);
+  count = Math.round(score_values.length/percent_val)
+  if (direction === 'forward'){
+      htmlCount = document.getElementById('forward_count');
+  }
+  else{
+    htmlCount = document.getElementById('reverse_count');
+  }
+
+  htmlCount.value = count;
+
+
+
 }
 
-function update_count_individual(direction, seq_id, percent_val){
+function update_count_individual(direction, seq_id, percent_val) {}
 
-}
-
-
-function submit_primers(direction){
+function submit_primers(direction) {
   // TODO
 }
